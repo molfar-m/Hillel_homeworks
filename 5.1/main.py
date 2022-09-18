74e57d356cfced01499df644782525710763bd8c
molfar-m <113460424+molfar-m@users.noreply.github.com>
2022-09-18 17:33:25 +0300

    add if function

---
+++ 5.1/main.py
@@ -0,0 +1,7 @@
+a = float(input('Введіть число a:'))
+b = float(input('Введіть число b:'))
+c = float(input('Введіть число c:'))
+if a > 10 and b > 10 and c > 10 and a % 3 == 0 and b % 3 == 0:
+    print ('yes')
+else:
+    print ('no')
