90eb188eda82828bbd35c6642601694f2c9673a1
molfar-m <113460424+molfar-m@users.noreply.github.com>
2022-10-01 06:55:14 +0300

    add minimum pass length

--- 8/8.4.py
+++ 8/8.4.py
@@ -3,6 +3,9 @@ from random import choice
 chars = (ascii_lowercase + ascii_uppercase + digits + punctuation)
 symbol_lance = int(input('Введіть довжину для генерації пароля: '))
 password = ''
-for i in range(symbol_lance):
-    password += choice(chars)
-print(f'Пароль: {password}')
+if symbol_lance >= 9:
+    for i in range(symbol_lance):
+        password += choice(chars)
+    print(f'Пароль: {password}')
+else:
+    print('Мінімальна довжина пароля повинна складати 9 символів')
