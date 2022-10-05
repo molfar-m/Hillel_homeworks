3902abe27ca5a6be56d275013717bd9a59e0c786
molfar-m <113460424+molfar-m@users.noreply.github.com>
2022-10-05 20:45:01 +0300

    correct code with conditions from exersise #3

--- 8/8.4.py
+++ 8/8.4.py
@@ -1,11 +1,17 @@
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
-from random import choice
-chars = (ascii_lowercase + ascii_uppercase + digits + punctuation)
-symbol_lance = int(input('Введіть довжину для генерації пароля: '))
-password = ''
-if symbol_lance >= 9:
-    for i in range(symbol_lance):
-        password += choice(chars)
-    print(f'Пароль: {password}')
+from random import choices, choice, shuffle
+symbol_len = int(input('Введіть число символів для генерації пароля: '))
+if symbol_len >= 4:
+    chars_list = [ascii_lowercase, ascii_uppercase, digits, punctuation] * (symbol_len // 4)
+    chars_list.extend(choices(chars_list, k=symbol_len % 4))
+    password = ''
+    for r in chars_list:
+        password += choice(r)
+    password = list(password)
+    shuffle(password)
+    mixed_password = ''
+    for n in password:
+        mixed_password += n
+    print(mixed_password)
 else:
-    print('Мінімальна довжина пароля повинна складати 9 символів')
+    print('Мінімальна довжина пароля повинна складати 4 символи!')
