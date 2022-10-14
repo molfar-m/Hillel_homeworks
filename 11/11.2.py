81195d074a18c185baf4926a947f4b951757f06c
molfar-m <113460424+molfar-m@users.noreply.github.com>
2022-10-14 15:16:07 +0300

    done 2nd commit

--- 11/11.2.py
+++ 11/11.2.py
@@ -1,17 +1,17 @@
 access_rights = {}
 n = int(input('Введіть к-сть файлів для перевірки, n: '))
 for i in range(n):
-    l = input(f'Введіть назву файлу #{i + 1} та допустимі команди: ').split()
-    access_rights[l[0]] = set(l[1:])
+    files_input = input(f'Введіть назву файлу #{i + 1} та допустимі команди: ').split()
+    access_rights[files_input[0]] = set(files_input[1:])
 for k in range(int(input('Введіть к-сть запитів до файлів, m: '))):
-        a_r, request = input(f'Введіть потрібну команду та назву файлу, запит #{k + 1}: ').split()
-        if a_r == 'write':
-            a_r = 'W'
-        if a_r == 'read':
-           a_r = 'R'
-        if a_r == 'execute':
-            a_r = 'X'
-        if a_r in access_rights[request]:
-            print('OK')
-        else:
-            print('Access denied')
+    ar, request = input(f'Введіть потрібну команду та назву файлу, запит #{k + 1}: ').split()
+    if ar == 'write':
+        ar = 'W'
+    if ar == 'read':
+        ar = 'R'
+    if ar == 'execute':
+        ar = 'X'
+    if ar in access_rights[request]:
+        print('OK')
+    else:
+        print('Access denied')
