d95c7ac105eec6ea113f0c1b5a04327ca9f43a44
molfar-m <113460424+molfar-m@users.noreply.github.com>
2022-10-18 22:39:45 +0300

    merging dict

---
+++ 12/12.2.py
@@ -0,0 +1,11 @@
+from json import dump
+A = {'key': 1, 'key2': True}
+B = {'key': 'Hello'}
+C = dict.copy(A)
+for i, j in B.items():
+    if not C.get(i):
+        C[i] = j
+    else:
+        C[i] = [C[i], j]
+with open('dic_c.json', 'w') as k:
+    dump(C, k)
